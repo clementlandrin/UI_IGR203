076be095d3f981c1228514b0b6a97a997fa5fcc7
clandrin <clement.landrin@telecom-paristech.fr>
2019-03-25 11:04:09 +0100

    The four choice menu is now appearing and disappearing as it should in the final version.

--- app/src/main/java/com/example/ui_igr203/TableActivity.java
+++ app/src/main/java/com/example/ui_igr203/TableActivity.java
@@ -5,7 +5,9 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -55,17 +57,63 @@ public class TableActivity extends AppCompatActivity implements View.OnClickList
 
     @Override
     public void onClick(View v) {
+        // useless for now as dispatchTouchMethod hide
         switch(v.getId())
         {
             case R.id.center_table:
-                if (four_choice_menu.getVisibility() == View.INVISIBLE)
+                break;
+        }
+    }
+
+    private boolean checkIsOnCenterTable(float x, float y) {
+        boolean b = false;
+        float density = getResources().getDisplayMetrics().density;
+
+        int[] location = new int[2];
+
+        center_table.getLocationOnScreen(location);
+        Log.i("check is on table","event::x is : "+Float.toString(x));
+        Log.i("check is on table","event::y is : "+Float.toString(y));
+
+        if(center_table != null)
+        {
+            if (( x > location[0]) && (x < location[0] + center_table.getWidth()*density/2))
+            {
+                if (( y > location[1]) && (y < location[1] + center_table.getHeight()*density/2))
+                {
+                    b = true;
+                }
+            }
+        }
+        return b;
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        int action = event.getActionMasked();
+        Log.i("onTouchEvent","trigerred");
+        switch(action) {
+            case MotionEvent.ACTION_DOWN:
+                Log.i("onTouchEvent","ACTION_DOWN");
+                if (checkIsOnCenterTable(event.getX(), event.getY()) && four_choice_menu != null)
                 {
                     four_choice_menu.setVisibility(View.VISIBLE);
                 }
-                else if (four_choice_menu.getVisibility() == View.VISIBLE)
+                break;
+            case MotionEvent.ACTION_MOVE:
+                Log.i("onTouchEvent","ACTION_MOVE");
+                break;
+            case MotionEvent.ACTION_UP:
+                Log.i("onTouchEvent","ACTION_UP");
+                if (!checkIsOnCenterTable(event.getX(), event.getY()) && four_choice_menu != null)
                 {
                     four_choice_menu.setVisibility(View.INVISIBLE);
                 }
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                Log.i("onTouchEvent","ACTION_CANCEL");
+                break;
         }
+        return super.dispatchTouchEvent(event);
     }
 }
